d1c0d7f7e0f84fa02fccb39c6d428528c72e42aa
navyjt <navyjt@163.com>
2018-01-18 12:46:31 +0800

    增加邮件正文的html表示格式 同时修改两个邮件模板
    Signed-off-by: navyjt <navyjt@163.com>

--- app/sendemail.py
+++ app/sendemail.py
@@ -15,7 +15,7 @@ def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-
+#此函数用来异步发送邮件
 def send_async_email(app,smtpserver,fromaddress,toaddress,msg):
     with app.app_context():
         smtpserver.sendmail(fromaddress,toaddress,msg)
@@ -44,9 +44,14 @@ def send_email(to, subject, template, **kwargs):
     msg=MIMEMultipart()
 
     body = render_template(template + '.txt', **kwargs)
+    html = render_template(template + '.html', **kwargs)
     
-    #此处一定要设置为gb2312，才能解决邮件正文的中文显示问题
-    body = MIMEText(body, 'plain', 'gb2312')
+    #此处一定要设置为gb2312，才能解决邮件正文的中文显示问题,html表示可以发送html格式的邮件正文@20180118
+    #这里将内容通过html模板渲染后传入msg的body体中，若将body和html都传入，html将作为附件显示，若将html
+    #作为body传入，则邮件正文显示为html网页形式，这才是我们想要的
+    body = MIMEText(html, 'html', 'gb2312')
+    # html = MIMEText(html, 'html', 'gb2312')
+
 
     # body.set_charset("utf-8")
     msg['From'] = _format_addr('Python用户 <%s>' % fromaddress)
@@ -54,6 +59,7 @@ def send_email(to, subject, template, **kwargs):
     msg['Subject'] = Header(subject, 'utf-8').encode()
 
     msg.attach(body)
+    # msg.attach(html)
 
     # 多线程调用
     thr = Thread(target=send_async_email, args=[app,smtpserver,fromaddress,toaddress,msg.as_string()])
